62acb0e490226b8699d35f7764e298cd4e051ad0
Yoshiyasu KO <te108128@gmail.com>
2021-06-01 22:24:16 +0900

    implement article via json serializable

---
+++ lib/domain/entity/article.dart
@@ -0,0 +1,21 @@
+import 'package:flutter_retrofit_sample/domain/entity/user.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'article.g.dart';
+
+@JsonSerializable(explicitToJson: true)
+class Article {
+  String id;
+  String title;
+  String url;
+  User user;
+
+  Article(
+      {required this.id,
+      required this.title,
+      required this.url,
+      required this.user});
+
+  factory Article.fromJson(Map<String, dynamic> json) =>
+      _$ArticleFromJson(json);
+}
