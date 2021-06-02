4320a0a9133bcfdf5bb5596911c0e86f90a922cf
Yoshiyasu KO <te108128@gmail.com>
2021-06-02 19:00:38 +0900

    implement article_detail_web_view

---
+++ lib/view/article_detail_web_view.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_retrofit_sample/domain/entity/article.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class ArticleDetailWebView extends StatelessWidget {
+  final Article article;
+
+  ArticleDetailWebView({Key? key, required this.article}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          leading: BackButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+          ),
+          title: Text(article.title),
+        ),
+        body: Center(
+          child: WebView(initialUrl: article.url),
+        ),
+      ),
+    );
+  }
+}
