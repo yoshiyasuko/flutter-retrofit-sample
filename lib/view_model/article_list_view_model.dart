c0ba9e2e4f8cc729528e10732a19d0d18364535b
Yoshiyasu KO <te108128@gmail.com>
2021-06-02 18:35:56 +0900

    implement article_list_view_model

---
+++ lib/view_model/article_list_view_model.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/widgets.dart';
+import 'package:flutter_retrofit_sample/domain/entity/article.dart';
+import 'package:flutter_retrofit_sample/domain/repository/article_repository.dart';
+import 'package:flutter_retrofit_sample/domain/repository/article_repository_impl.dart';
+
+class ArticleListViewModel with ChangeNotifier {
+  final ArticleRepository repository = ArticleRepositoryImpl();
+  late final List<Article> articles;
+
+  ArticleListViewModel() {
+    this.articles = [];
+    fetchArticles();
+  }
+
+  void fetchArticles() async {
+    await repository.fetchArticles().then((result) {
+      result.when(success: (articles) {
+        this.articles = articles;
+        notifyListeners();
+      }, failure: (error) {
+        print(error.getErrorMessage());
+      });
+    });
+  }
+}
